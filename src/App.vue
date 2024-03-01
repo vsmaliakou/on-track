4f47505946f0db60585d12b52ef5075c1ab46df2
vsmaliakou <vsmaliakou@yandex.by>
2024-03-01 21:09:33 +0300

    FEAT: Add Logo, HeaderProgress components

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,14 @@
 <script setup>
-import { CheckCircleIcon } from '@heroicons/vue/24/solid';
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
-
-const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon,
-};
+import Header from '@/components/Header.vue';
+import Nav from '@/components/Nav.vue';
 </script>
 
 <template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    <a href="#">
-      <img src="./assets/logo.png" alt="Logo" class="h-9" />
-    </a>
-
-    <a href="#" class="text-sm">
-      <div v-if="true" class="flex items-center gap-1">
-        Day complete!
-
-        <CheckCircleIcon class="h-7 text-green-500" />
-      </div>
-
-      <div v-else class="flex items-center gap-1">
-        <div>Progress: <span class="font-mono">20%</span></div>
-
-        <div class="h-3 w-3 rounded-full bg-red-500"></div>
-      </div>
-    </a>
-  </header>
+  <Header />
 
   <main class="flex flex-grow flex-col">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aliquid!
   </main>
 
-  <nav class="sticky bottom-0 z-10 bg-white">
-    <ul class="flex items-center justify-around border-t">
-      <li v-for="(icon, item) in navItems" :key="item" class="flex-1">
-        <a class="flex flex-col items-center p-2 text-xs capitalize" :href="`#${item}`">
-          <component :is="icon" class="h-6 w-6" />
-          
-          {{ item }}
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <Nav />
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Logo from '@/components/Logo.vue';
+import HeaderProgress from '@/components/HeaderProgress.vue';
+</script>
+
+<template>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <Logo />
+
+    <HeaderProgress />
+  </header>
+</template>
