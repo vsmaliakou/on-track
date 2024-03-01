e73372ccd5d97f2aee0b784784979b20312e2a00
vsmaliakou <vsmaliakou@yandex.by>
2024-03-01 22:29:14 +0300

    FEAT: Page Components

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,27 +1,16 @@
 <script setup>
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import NavItem from '@/components/NavItem.vue'
-import { ref } from 'vue'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '@/const'
+
+defineProps(['currentPage'])
+
+const emit = defineEmits(['navigate'])
 
 const navItems = {
   [PAGE_TIMELINE]: ClockIcon,
   [PAGE_ACTIVITIES]: ListBulletIcon,
   [PAGE_PROGRESS]: ChartBarIcon
-}
-
-const currentPage = ref(normalizePageHash())
-
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1)
-
-  if (Object.keys(navItems).includes(hash)) {
-    return hash
-  }
-
-  window.location.hash = PAGE_TIMELINE
-
-  return PAGE_TIMELINE
 }
 </script>
 
@@ -33,7 +22,7 @@ function normalizePageHash() {
         :key="item"
         :href="`#${item}`"
         :class="{ 'bg-gray-200 pointer-events-none': currentPage === item }"
-        @click="currentPage = item"
+        @click="emit('navigate', item)"
       >
         <component :is="icon" class="h-6 w-6" /> {{ item }}
       </NavItem>
