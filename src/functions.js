cfd318642be7a8bbab2ed8e422d6ba79ab985bff
vsmaliakou <vsmaliakou@yandex.by>
2024-03-03 10:51:43 +0300

    FEAT: Generating Timeline Data

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,4 @@
-import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from '@/const'
+import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE, HOURS_IN_DAY } from '@/const'
 
 export function normalizePageHash() {
   const hash = window.location.hash.slice(1)
@@ -10,4 +10,14 @@ export function normalizePageHash() {
   window.location.hash = PAGE_TIMELINE
 
   return PAGE_TIMELINE
+}
+
+export function generateTimelineItems() {
+  const timelineItems = []
+
+  for (let hour = 0; hour < HOURS_IN_DAY; hour++) {
+    timelineItems.push({ hour })
+  }
+
+  return timelineItems
 }
