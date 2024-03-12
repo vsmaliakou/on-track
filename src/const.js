cd570395697609c5317a2e85a27d4d72452c075c
vsmaliakou <vsmaliakou@yandex.by>
2024-03-12 19:43:08 +0300

    FEAT: Stopwatch markings

--- src/const.js
+++ src/const.js
@@ -26,8 +26,16 @@ export const PERIOD_SELECT_OPTIONS = generatePeriodSelectOptions(PERIODS_IN_MINU
 export const BUTTON_TYPE_DANGER = 'danger'
 export const BUTTON_TYPE_NEUTRAL = 'neutral'
 export const BUTTON_TYPE_PRIMARY = 'primary'
+export const BUTTON_TYPE_SUCCESS = 'success'
+export const BUTTON_TYPE_WARNING = 'warning'
 
-export const BUTTON_TYPES = [BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_PRIMARY]
+export const BUTTON_TYPES = [
+  BUTTON_TYPE_DANGER,
+  BUTTON_TYPE_NEUTRAL,
+  BUTTON_TYPE_PRIMARY,
+  BUTTON_TYPE_SUCCESS,
+  BUTTON_TYPE_WARNING
+]
 
 export const NULLABLE_ACTIVITY = {
   id: null
