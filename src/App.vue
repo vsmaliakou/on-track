bc366e3ea4358edf529f4377069f88618a25ee2d
vsmaliakou <vsmaliakou@yandex.by>
2024-03-18 22:31:51 +0300

    FEAT: timeline-items module

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import Timeline from '@/pages/Timeline.vue'
 import Activities from '@/pages/Activities.vue'
 import Progress from '@/pages/Progress.vue'
 import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from '@/const'
-import { provide, readonly, ref } from 'vue'
-import { generateTimelineItems, generatePeriodSelectOptions } from '@/functions'
+import { provide, readonly } from 'vue'
+import { generatePeriodSelectOptions } from '@/functions'
 import { currentPage, timelineRef } from '@/router'
 import * as keys from '@/keys'
 import {
@@ -16,16 +16,11 @@ import {
   deleteActivity,
   activities
 } from '@/activities'
-
-const timelineItems = ref(generateTimelineItems(activities.value))
-
-function setTimelineItemActivity(timelineItem, activityId) {
-  timelineItem.activityId = activityId
-}
-
-function updateTimelineItemActivitySeconds(timelineItem, activitySeconds) {
-  timelineItem.activitySeconds += activitySeconds
-}
+import {
+  timelineItems,
+  updateTimelineItemActivitySeconds,
+  setTimelineItemActivity
+} from '@/timeline-items'
 
 provide(keys.timelineItemsKey, readonly(timelineItems))
 provide(keys.activitySelectOptionsKey, readonly(activitySelectOptions))
