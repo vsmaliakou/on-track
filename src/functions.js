bf74c1612461778b123170ec6deb75e22c64d1d8
vsmaliakou <vsmaliakou@yandex.by>
2024-03-18 22:42:15 +0300

    FEAT: Fixing the delete activities function

--- src/functions.js
+++ src/functions.js
@@ -1,23 +1,10 @@
 import {
-  HOURS_IN_DAY,
   SECONDS_IN_HOUR,
   MINUTES_IN_HOUR,
   SECONDS_IN_MINUTE,
   MILLISECONDS_IN_SECOND
 } from '@/const'
 import { isNull } from '@/validators'
-
-export function generateTimelineItems(activities) {
-  return [...Array(HOURS_IN_DAY).keys()].map((hour) => {
-    return {
-      hour,
-      // activityId: hour % 4 === 0 ? null : activities[hour % 2].id,
-      // activitySeconds: hour % 4 === 0 ? 0 : (15 * SECONDS_IN_MINUTE * hour) % SECONDS_IN_HOUR
-      activityId: [0, 1, 2, 3, 4].includes(hour) ? activities[hour % 3].id : null,
-      activitySeconds: [0, 1, 2, 3, 4].includes(hour) ? hour * 600 : 0
-    }
-  })
-}
 
 export function generateActivitySelectOptions(activities) {
   return activities.map((activity) => ({ label: activity.name, value: activity.id }))
