ad2b7ef36c5a18a5b20988942bfe0f6bfb096434
vsmaliakou <vsmaliakou@yandex.by>
2024-03-27 16:16:45 +0300

    FEAT: Refactoring the storage module

--- src/main.js
+++ src/main.js
@@ -2,24 +2,7 @@ import '@/assets/main.css'
 
 import { createApp } from 'vue'
 import App from '@/App.vue'
-import * as storage from '@/storage'
-import { timelineItems } from '@/timeline-items'
-import { activities } from '@/activities'
-import { isToday } from '@/time'
-
-function saveState() {
-  storage.save({
-    timelineItems: timelineItems.value,
-    activities: activities.value
-  })
-}
-
-function loadState() {
-  const state = storage.load()
-
-  timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value
-  activities.value = state.activities || activities.value
-}
+import { loadState, saveState } from '@/storage'
 
 loadState()
 
